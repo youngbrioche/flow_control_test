47e7d3e52dd7a35f0c89a95effc25efd65e62761
Philipp Haußleiter <philipp@haussleiter.de>
2012-08-01 01:52:58 +0200

    updating settings for local FlowControl Test

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
-  config.host = 'http://phl.innoq.com'
+  config.host = 'localhost'
   config.port = 9090
   config.secure = false
-  config.api_key = '3e9dfbb9-8711-4d6e-ba7a-0602e7e09316'
+  config.api_key = 'ca110f8e-999e-481b-afdf-161de9e62130'
 end
